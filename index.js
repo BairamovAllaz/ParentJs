1227a874232956469b7eeb2c8560243016c2857e
bairamov <ellezbayramov2004@gmail.com>
2023-02-16 14:15:33 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Example = Example;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function Example() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello world this is my libary"));
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Example()
+{
+    return(
+        <div>
+            <h1>Hello world this is my libary</h1>
+        </div>
+    )
+}
